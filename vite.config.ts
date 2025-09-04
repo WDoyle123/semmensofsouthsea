873486bc9a6f78413f91c0845d7da0e3e7a84e56
William Doyle <will.doyle30@gmail.com>
2025-09-05 00:47:38 +0100

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 
+const repo = process.env.GITHUB_REPOSITORY?.split("/")[1];
+const owner = process.env.GITHUB_REPOSITORY_OWNER;
+const isUserSite = repo === `${owner}.github.io`;
+
 export default defineConfig({
+  base: isUserSite ? "/" : `/${repo}/`,
   plugins: [tailwindcss(), react()],
 });
+
